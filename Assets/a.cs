50d56a2ae979419047b541b3dd0a9e6318e1713f
ZhangYuanze <2604761105@qq.com>
2022-08-31 12:03:43 +0800

    sss

--- Assets/a.cs
+++ Assets/a.cs
@@ -7,7 +7,7 @@ public class a : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i;
     }
 
     // Update is called once per frame
